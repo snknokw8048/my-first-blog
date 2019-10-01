4c39e1c2624166387becb994e52728c0f6d06739
Your Name <you@example.com>
2019-10-01 15:00:01 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone #20191001追加
+from .models import Post #20191001追加
 
 # Create your views here.
 
 #20191001追加
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
